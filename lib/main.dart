014bc41b2119d5803411595004536b90d8017087
user <sofiia.bykovska@chisw.com>
2022-12-01 17:33:58 +0200

    initial commit, basic structure

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:advicer_app/theme.dart';
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+import 'application/core/services/theme_service.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeService(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeService>(
+      builder: (context, themeService, child) {
+        return MaterialApp(
+          themeMode:
+              themeService.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          home: const Placeholder(),
+        );
+      },
+    );
+  }
+}
